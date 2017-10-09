4f8632a1603a7943e2e9cc193c70251892a993f7
Henri Utter <e1401162@shell.vamk.fi>
2017-10-09 11:40:42 +0300

    editointia

--- testcase.c
+++ testcase.c
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "ringbuffer.h"
-
+// jep jep
 /****************************************************************************/
 
 TEST(RingBuffer_init_buffer, first_init)
