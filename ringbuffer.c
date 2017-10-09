1b5ebde389b4e0ba54e694eb11d08e4ac5cc91a5
Henri Utter <e1401162@shell.vamk.fi>
2017-10-09 09:41:01 +0300

    eka versio

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,26 +1,124 @@
-//
-#include "include.h"
+#include "ringbuffer.h"
 
-void empty_buffer(struct buffer_type *b)
-{}
-void init_buffer(struct buffer_type *b, unsigned char *buffer)
-{}
-enum error_type create_buffer(struct buffer_type *b, int size)
-{}
-int add_char_to_buffer(struct buffer_type *b, unsigned char c,
-                                 enum error_type *err) {}
-unsigned char get_char_from_buffer(struct buffer_type *b,
-                                             enum error_type *err)
-{}
-int add_string_to_buffer(struct buffer_type *b, unsigned char *s,
-                         enum error_type *err)
-{}
-unsigned char get_string_from_buffer(struct buffer_type *b, unsigned char *s,
-                                     enum error_type *err)
-{}
-int get_buffer_state(struct buffer_type b)
-{}
-void print_buffer(struct buffer_type b)
-{}
-unsigned char *move_pointer_to_next(struct buffer_type *b, unsigned char *p)
-{}
+
+void init_buffer(struct buffer_type *b, unsigned char *buffer){	
+	b->head=buffer;
+	b->tail=buffer;
+	b->buffer=buffer;
+};
+
+void empty_buffer(struct buffer_type *b){
+	b->tail=b->head;	
+};
+signed int get_buffer_status(struct buffer_type b, int *err){
+	int amount = 0;
+	amount = b.head-b.tail;
+	if(amount<0){
+		amount=amount+MAX_BUFFER;
+	}
+	if (amount >=0 && amount <= MAX_BUFFER){
+		*err = OK;
+		return amount;
+	}
+	else{
+		*err = BUFFER_OVER_FLOW;
+		return -1;
+	}
+	
+};
+int add_char_to_buffer(struct buffer_type *b, unsigned char c, int *err){
+	if(b->head < (b->buffer+MAX_BUFFER-1)){
+		*(b->head) = c;
+		b->head++;
+	}
+	else if((b->head == (b->buffer+MAX_BUFFER-1)) && (b->head !=b->tail)){
+		*(b->head) = c;
+		b->head = b->buffer;		
+	}
+	else{
+		*err = BUFFER_FULL;
+		return -1;
+	}
+	return get_buffer_status(*b,err);
+};
+signed char get_char_from_buffer(struct buffer_type *b, int *err){
+	signed char temp;
+	if((b->tail < (b->buffer+MAX_BUFFER-1)) && (b->head !=b->tail)){
+		temp = *(b->tail);
+		b->tail++;
+	}
+	else if((b->tail == (b->buffer+MAX_BUFFER-1))){
+		temp = *(b->tail);
+		b->tail = b->buffer;		
+	}
+	else{
+		*err = POINTER_ERROR;
+		return -1;
+	}
+	return temp;
+};
+int print_buffer(struct buffer_type b, int *err){
+	unsigned char *h = b.head;
+	unsigned char *t = b.tail;
+	char temp,i = 0;
+	printf("\n\n");
+	while(t != h){
+		if((t < (b.buffer+MAX_BUFFER-1)) && (h != t)){
+			temp = *t;
+			t++;
+		}
+		else if((t == (b.buffer+MAX_BUFFER-1))){
+			temp = *t;
+			t = b.buffer;		
+		}
+		else{
+			*err = POINTER_ERROR;
+			return -1;
+		}
+		printf("%c",temp);
+		i++;
+	}
+	printf("\n\n");
+	
+	return i;
+	
+};
+int add_string_to_buffer(struct buffer_type *b, unsigned char *s, int *err){
+	int i = 0;
+	int l = 0;
+	while(*(s+l) != '\0'){
+		l++;
+	};
+	int k = get_buffer_status(*b,err);
+	
+	if(l > (MAX_BUFFER-k)){
+		return -1;
+	}
+	
+	while(*(s+i) != '\0'){
+		if(b->head < (b->buffer+MAX_BUFFER-1)){
+			*(b->head) = *(s+i);
+			b->head++;
+		}
+		else if((b->head == (b->buffer+MAX_BUFFER-1)) && (b->head !=b->tail)){
+			*(b->head) = *(s+i);
+			b->head = b->buffer;		
+		}
+		else{
+			*err = BUFFER_FULL;
+			return -1;
+		}
+		i++;
+	};
+	return i;
+	
+};
+int  get_string_from_buffer(struct buffer_type *b, unsigned char *dest, int len, int *err){
+	int i=0;
+	
+	for(i=0; i<len;i++){
+		*(dest+i)=get_char_from_buffer(b,err);
+	}
+	return i;
+	
+};
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,48 +1,20 @@
-#include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#define MAX_BUFFER 10
 
-#define MAX_BUFFER 10 ///< Length of global buffer space
-
-#define OK 1     ///< OK
-#define ERROR -1 ///< Error
-
-#define BUFF_LEN(a, b) a < b ? b - a : a - b
-
-//! \enum error_type enumerators for error conditions
-enum error_type {
-  BUFF_OK,        /*! No error */
-  BUFFER_EMPTY,   /*! Buffer is empty */
-  BUFFER_FULL,    /*! Buffer is full */
-  BUFFER_OVERRUN, /*! Try to read over buffer end */
-  BUFFER_ERROR,
-  UNKNOWN_ERROR
+struct buffer_type{
+  unsigned char *head;
+  unsigned char *tail;
+  unsigned char *buffer;
 };
 
-/*
-\struct buffer_type
-*/
-struct buffer_type {
-  unsigned char *head;   ///< Current adding position of the buffer
-  unsigned char *tail;   ///< Currently last entry
-  unsigned char *buffer; ///< Pointer to buffer
-};
+enum error_type{OK,BUFFER_OVER_FLOW,EMPTY_BUFFER,POINTER_ERROR,BUFFER_FULL};
 
-void empty_buffer(struct buffer_type *b);
 void init_buffer(struct buffer_type *b, unsigned char *buffer);
-enum error_type create_buffer(struct buffer_type *b, int size);
-int add_char_to_buffer(struct buffer_type *b, unsigned char c, enum error_type *err);
-unsigned char get_char_from_buffer(struct buffer_type *b, enum error_type *err);
-int add_string_to_buffer(struct buffer_type *b, unsigned char *s, enum error_type *err);
-unsigned char get_string_from_buffer(struct buffer_type *b, unsigned char *s,
-                            enum error_type *err);
-int get_buffer_state(struct buffer_type b);
-void print_buffer(struct buffer_type b);
-unsigned char *move_pointer_to_next(struct buffer_type *b, unsigned char *p);
-
-enum error_type is_buffer_full(struct buffer_type *b);
-enum error_type is_buffer_empty(struct buffer_type *b);
-int get_buffer_length(struct buffer_type *b);
-
-extern unsigned char transmission_buffer[MAX_BUFFER];
-extern unsigned char recive_buffer[MAX_BUFFER];
+void empty_buffer(struct buffer_type *b);
+signed int get_buffer_status(struct buffer_type b, int *err);
+int add_char_to_buffer(struct buffer_type *b, unsigned char c, int *err);
+signed char get_char_from_buffer(struct buffer_type *b, int *err);
+int print_buffer(struct buffer_type b, int *err);
+int add_string_to_buffer(struct buffer_type *b, unsigned char *s, int *err);
+int  get_string_from_buffer(struct buffer_type *b, unsigned char *dest, int len, int *err);
